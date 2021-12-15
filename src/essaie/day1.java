31b77ce87b50ecb0d3352d1be0a6ca25635b2b3e
aalarie134 <91813705+aalarie134@users.noreply.github.com>
2021-12-14 19:06:07 -0500

    read file works

--- src/essaie/day1.java
+++ src/essaie/day1.java
@@ -1,10 +1,23 @@
 package essaie;
+import java.io.*;
+import java.util.Scanner;
+
 
 public class day1 {
-    public static void main(String[] args){
-        System.out.println("helloworld");
+    public static void main(String[] args) throws IOException {
+        int answer = 0;
+        File file = new File("input.txt");
 
-        System.out.println(" test commint in IJ");
+        Scanner scan = new Scanner(file);
+        int max = (int) file.length();
+        System.out.println(max);
+        int temp = scan.nextInt();
+        System.out.println(temp);
+        for(int i = 0 ; i < max ; i++){
+                //to do
+        }
+
+
 
 
     }
