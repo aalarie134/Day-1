78ef07d48d162f1fed103e13bf78b51fdbee862c
aalarie134 <91813705+aalarie134@users.noreply.github.com>
2021-12-14 21:01:46 -0500

    completed with good answer part 2

--- src/essaie/day1.java
+++ src/essaie/day1.java
@@ -11,18 +11,31 @@ public class day1 {
         Scanner scan = new Scanner(file);
         long max =  1999;
 
-        int temp = scan.nextInt();
+        int temp1 = scan.nextInt();
+        int temp2 = scan.nextInt();
+        int temp3 = scan.nextInt();
 
-        for(int i = 1 ; i < max + 1 ; i++){
-            int a = temp;
-            int b = scan.nextInt();
 
-            if( comparator.compare(a,b) ){
+        for(int i = 3 ; i < max + 1 ; i++){
+            int a = temp1;
+            int b = temp2;
+            int c = temp3;
+
+            int sumA = a + b + c;
+
+            int d = scan.nextInt();
+
+            int sumB = b + c + d;
+
+
+            if( comparator.compare(sumA,sumB) ){
 
                 answer++;
             }
 
-            temp = b;
+            temp1 = b;
+            temp2 = c;
+            temp3 = d;
         }
 
         System.out.println(answer);
